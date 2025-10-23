4e2317933286f10ed96e2eb9b3bb65cd7fbd784e
Shane McDonough <shanemcdman@gmail.com>
2025-10-23 15:43:09 -0700

    add some text

--- content-script.js
+++ content-script.js
@@ -8,18 +8,39 @@ function newEl(tag, parent = null) {
 	return el;
 }
 
+function newP(text, parent = null) {
+	const el = newEl('p', parent);
+	el.innerText = text;
+	return el;
+}
+
 function createPopup() {
+	popup = newEl('div', document.body);
+	popup.style.position = 'absolute';
+	popup.style.left = 0;
+	popup.style.top = 0;
+	popup.style.width = '100vw';
+	popup.style.height = '100vh';
+	popup.style.background = '#999';
+	popup.style.opacity = 0.2;
+	popup.style['z-index'] = 1000000;
 	const div = newEl('div', document.body);
-	popup = div;
 	div.style.position = 'absolute';
 	div.style.left = '10vw';
 	div.style.top = '10vh';
 	div.style.width = '80vw';
 	div.style.height = '80vh';
-	div.style.background = 'black';
-	div.style['z-index'] = 1000000;
+	div.style.background = '#111';
+	div.style.color = '#ddd';
+	div.style['z-index'] = 1000001;
 	div.style['text-align'] = 'center';
 	div.style['border-radius'] = '1rem';
+	div.style['font-size'] = '2rem';
+	const h1 = newEl('h1', div);
+	h1.innerText = 'Hey there bud.';
+	h1.style.margin = '1rem';
+	newP('It looks like you\'re doomscrolling again.', div);
+	newP('Are you sure that\'s what you want to do?', div);
 }
 
 function removePopup() {
