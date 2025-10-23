6b83ab452af899e7e80253ef434bfa3511cd70f0
Shane McDonough <shanemcdman@gmail.com>
2025-10-23 15:28:20 -0700

    create popup on shorts and remove on youtube homepage

--- content-script.js
+++ content-script.js
@@ -1,3 +1,5 @@
+let popup = null;
+
 function newEl(tag, parent = null) {
 	const el = document.createElement(tag);
 	if(parent) {
@@ -6,14 +8,29 @@ function newEl(tag, parent = null) {
 	return el;
 }
 
-// set up list in top left
-(() => {
+function createPopup() {
 	const div = newEl('div', document.body);
+	popup = div;
 	div.style.position = 'absolute';
 	div.style.left = '10vw';
 	div.style.top = '10vh';
 	div.style.width = '80vw';
 	div.style.height = '80vh';
 	div.style.background = 'black';
+	div.style['z-index'] = 1000000;
 	div.style['text-align'] = 'center';
-})();
+}
+
+function removePopup() {
+	popup.remove();
+	popup = null;
+}
+
+setInterval(() => {
+	const shorts = document.location.pathname.includes('/shorts/');
+	if(popup && !shorts) {
+		removePopup();
+	} else if(!popup && shorts) {
+		createPopup();
+	}
+}, 100);
